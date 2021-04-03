72f2658268b1cb59d88a0a78a84155625c63bafe
Emanuel Filipe <efilipecode@gmail.com>
2021-04-03 13:37:17 -0300

    decorators

---
+++ decorators.py
@@ -0,0 +1,12 @@
+def announce(function):
+    def wrapper():
+        print("About to run the function")
+        function()
+        print("Done with the function")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello, world")
+
+hello()
