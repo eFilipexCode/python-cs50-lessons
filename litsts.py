7c4ccf9d5f3bcbfeb01ecd07e374400caec07930
Emanuel Filipe <efilipecode@gmail.com>
2021-04-02 23:54:36 -0300

    some sequences

---
+++ litsts.py
@@ -0,0 +1,7 @@
+names = ["Harry", "Ron", "Emmanuel", "Phillip"]
+
+# We can add  something to the list
+names.append("Draco")
+
+# Sort the items
+names.sort()
---
+++ sets.py
@@ -0,0 +1,14 @@
+# Create an empty set
+mySet = set()
+
+mySet.add(1)
+mySet.add(2)
+
+# {1, 2}
+
+mySet.remove(2)
+
+# {1}
+
+print(f"The set has {len(mySet)} elements")
+#len(mySet) gets the length of the set
