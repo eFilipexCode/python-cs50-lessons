23bc1fa90529eefdabbe4288521cd0ca8799c1eb
Emanuel Filipe <efilipecode@gmail.com>
2021-04-03 00:10:39 -0300

    functiions and imports

---
+++ functions.py
@@ -0,0 +1,5 @@
+# Defining the function
+def square(x):
+    return x * x
+
+# check sqaure.py to see imports
---
+++ square.py
@@ -0,0 +1,9 @@
+from function import square
+# or
+import functions
+
+for i in range(10):
+    print(square(i+1))
+# or
+for i in range(10):
+    print(functions.square(i+1))
