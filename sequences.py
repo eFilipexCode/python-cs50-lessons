a17ab9f5e2f2cc1d794f9b15f32e48f0e50fc7cd
Emanuel Filipe <efilipecode@gmail.com>
2021-04-02 23:47:00 -0300

    first glance at sequences

---
+++ sequences.py
@@ -0,0 +1,19 @@
+# string
+name = "Emanuel"
+print(name[0]) 
+# [0] takes the sequence and gives access to a particular value (similar to js)
+
+# Lists
+names = ["Harry", "Emanuel", "Brian"]
+print(names)
+print(names[0])
+print(names[1][0])
+
+#Tuple
+coordinates = (10.0, 20.0)
+
+#Data structures
+    # 1 - Lists => sequence of mutable values
+    # 2 - Tuples => sequence of immutable values
+    # 1 - Set => collection of unique values
+    # 1 - Dict(ionary) => collection of key-value values
