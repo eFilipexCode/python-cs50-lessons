e7fef93fd5599c1e42e5d34a210ad38556dada63
Emanuel Filipe <efilipecode@gmail.com>
2021-04-02 23:38:42 -0300

    python conditions and int function

---
+++ conditions.py
@@ -0,0 +1,9 @@
+n = int(input("Number: "))
+# n is a string, so we need to convert it
+
+if n > 0:
+    print("n is positive") 
+elif n < 0:
+    print("n is nagative")
+else: 
+    print("n is zero")
