ead17bc4201dfd8e0697f1c79c9586770f817abe
Emanuel Filipe <efilipecode@gmail.com>
2021-04-02 23:59:51 -0300

    loops

---
+++ looping.py
@@ -0,0 +1,12 @@
+# simplest loop
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+# or
+for i in range(6):
+    print(i)
+
+# Another example
+name = input("Type your name: ")
+for character in name:
+    print(character)
